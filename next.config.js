64b3b592b0edb9330fb821f6b024de9d330e006e
Jennifer <40246010+jnguyenbee@users.noreply.github.com>
2021-08-27 13:26:48 -0500

    set up db & api

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    MONGO_URI: "mongodb+srv://dev_admin:dev_app000@penny-for-thoughts.ryaiy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  },
 }
